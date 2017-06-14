4ebcde660e2b215ecb0d6f9c4a87758395852c3b
Tyler McGee <tylermcgee@tylers-macbook-air.local>
2017-06-14 10:50:31 -0400

    Committed 6/14/17.

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,7 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBAction func fadeIn(_ sender: Any) {
+        
+        image.alpha = 0
+        
+        UIView.animate(withDuration: 1, animations: {
+            
+            self.image.alpha = 1
+        })
+        
+    }
+    
+    
+    @IBAction func slideIn(_ sender: Any) {
+        
+        image.center = CGPoint(x: image.center.x - 500, y: image.center.y)
+        
+        UIView.animate(withDuration: 2, animations: {
+            
+            self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
+        })
+        
+    }
+    
+    
+    @IBAction func grow(_ sender: Any) {
+        
+        image.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+        UIView.animate(withDuration: 1, animations: {
+            
+            self.image.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        })
+        
+    }
+    
+    
+    @IBOutlet weak var button: UIButton!
 
+    @IBOutlet weak var image: UIImageView!
+    
+    var counter = 1
+    var isAnimating = false
+    
+    var timer = Timer()
+    
+    func animate() {
+        
+        image.image = UIImage(named: "frame_\(counter)_delay-0.07s.gif")
+        
+        counter += 1
+        
+        if counter == 32 {
+            
+            counter = 0
+        }
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        
+        if isAnimating {
+            
+            timer.invalidate()
+            
+            button.setTitle("Start Animation", for: [])
+            
+            isAnimating = false
+            
+        } else {
+        
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+            
+            button.setTitle("Stop Animation", for: [])
+            
+            isAnimating = true
+        
+            
+        }
+        
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
